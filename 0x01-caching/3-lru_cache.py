2ec7afdc6c0bb7410380e960e3c4de6a3d57549d
dnjoe96 <donjoedbest@gmail.com>
2022-07-12 18:13:08 +0100

    Implement LRU deletion

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" LRUCache Module"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ Class Implementing LRU"""
+    used = {}
+
+    def __int__(self):
+        """ class constructor"""
+        super().__init__()
+
+    def put(self, key, item):
+        """ Method to add to cache
+        """
+        if key and item:
+            # print('PUT ITEM {} >>>'.format(key))
+            if not self.used.get(key):
+                self.used[key] = 0
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                itemKeyToDiscard = \
+                    sorted(self.used.items(), key=lambda x: x[1])[0][0]
+
+                self.cache_data.pop(itemKeyToDiscard)
+                self.used.pop(itemKeyToDiscard)
+                print('DISCARD: {}'.format(itemKeyToDiscard))
+                # print('>>>>>>>>>>>>>>>', itemKeyToDiscard)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieve an item from cache
+        """
+        if key:
+            # print('GET ITEM {} >>>'.format(key))
+            # print(self.used)
+            if self.used.get(key) is not None:
+                self.used[key] += 1
+            # print(self.used)
+            return self.cache_data.get(key)
