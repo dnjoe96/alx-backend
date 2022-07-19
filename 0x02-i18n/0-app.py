e1076c9f38653c9d9b12e9363bd72e13b6b8ffda
dnjoe96 <donjoedbest@gmail.com>
2022-07-19 21:52:29 +0100

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,10 +2,14 @@
 """ Module for view definition """
 from flask import Flask, render_template
 
-app = Flask()
+app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     """ Index function """
-    return render_template('index.html')
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
