5f9255dd7933cf386790ea339dc63038c217e9b3
dnjoe96 <donjoedbest@gmail.com>
2022-08-16 19:11:36 +0100

    Prepare task files

---
+++ 0x03-queuing_system_in_js/test.js
@@ -0,0 +1,10 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => console.log('Redis client not connected to the server: ', err));
+
+await client.connect();
+
+await client.set('key', 'value');
+const value = await client.get('key');
